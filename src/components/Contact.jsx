279cc3d386a0068625730b150194d3262723a282
MD Shabbir Hossain <shabbirhossain660@gmail.com>
2024-03-21 23:46:14 +0600

    email.js complete

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,24 +1,123 @@
-import {motion} from "framer-motion";
+import { motion } from "framer-motion";
 import emailjs from "@emailjs/browser";
-import {styles} from "../style";
+import { styles } from "../style";
 import { EarthCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
 import { slideIn } from "../utils/motion";
 import { useRef, useState } from "react";
+import toast, { Toaster } from "react-hot-toast";
 
 const Contact = () => {
+
+  // service_s086zxl
+  // template_lau62pa
+  // 4-Uo5ZMCgrDWYFY-8
+  // iozn0fg4AS1bcAh3au9D1
   const formRef = useRef();
   const [form, setForm] = useState({
-    name:"",
-    email:"",
-    message:""
+    name: "",
+    email: "",
+    message: ""
   });
   const [loading, setLoading] = useState(false);
-  const handleChange = (e)=>{};
-  const handleSubmit = (e)=>{}
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setForm({ ...form, [name]: value })
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
+    emailjs.send("service_s086zxl", "template_8hewqki",
+      {
+        from_name: form.name,
+        from_email: form.email,
+        to_name: "Shabbir",
+        to_email: "hossainshabbir51@gmail.com",
+        message: form.message
+      },
+      "4-Uo5ZMCgrDWYFY-8"
+    )
+      .then(() => {
+        setLoading(true);
+        toast.success("Thank you. I will get back with you as soon as possible");
+        setForm({
+          name: "",
+          email: "",
+          message: ""
+        }),
+        (error)=>{
+         setLoading(false);
+         toast.error(error.message);
+        }
+      })
+  }
   return (
-    <div>Contact</div>
+    <div className="xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden">
+      <motion.div
+        variants={slideIn("left", "tween", 0.2, 1)}
+        className="flex-[0.75] bg-black-100 p-8 rounded-2xl"
+      >
+        <p className={styles.sectionSubText}>Get In Touch</p>
+        <h3 className={styles.sectionHeadText}>Contact</h3>
+        <form
+          ref={formRef}
+          onSubmit={handleSubmit}
+          className="mt-12 flex flex-col gap-8"
+        >
+          <label className="flex flex-col">
+            <span className="text-white font-medium mb-4"> Your Name</span>
+            <input
+              type="text"
+              name="name"
+              value={form.name}
+              onChange={handleChange}
+              placeholder="What is your name?"
+              className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium"
+            />
+          </label>
+          <label className="flex flex-col">
+            <span className="text-white font-medium mb-4"> Your Email</span>
+            <input
+              type="text"
+              name="email"
+              value={form.email}
+              onChange={handleChange}
+              placeholder="What is your email?"
+              className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium"
+            />
+          </label>
+          <label className="flex flex-col">
+            <span className="text-white font-medium mb-4"> Your Message</span>
+            <textarea
+              // type="text"
+              rows="7"
+              name="message"
+              value={form.message}
+              onChange={handleChange}
+              placeholder="What do you want to say?"
+              className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium"
+            />
+          </label>
+          <button
+            type="submit"
+            className="bg-tertiary py-3 px-8 outline-none w-fit text-white font-bold shadow-md shadow-primary rounded-xl"
+          >
+            {loading ? "Sending..." : "Send"}
+          </button>
+        </form>
+      </motion.div>
+      <motion.div
+        variants={slideIn("right", "tween", 0.2, 1)}
+        className="xl:flex-1 xl:h-auto md:h-[550px] h-[350px]"
+      >
+        <EarthCanvas />
+      </motion.div>
+      <Toaster
+  position="top-center"
+  reverseOrder={false}
+/>
+    </div>
   )
 }
 
-export default Contact
+export default SectionWrapper(Contact, "contact");
--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -76,4 +76,4 @@ const Works = () => {
   )
 }
 
-export default SectionWrapper(Works, "");
+export default SectionWrapper(Works, "work");
